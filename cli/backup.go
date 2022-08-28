db008ea71ba52c01e1697f2d90ef68d174e36d03
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2022-08-28 15:44:12 -0300

    backup: Allow to execute a command

--- cli/backup.go
+++ cli/backup.go
@@ -2,7 +2,14 @@ package cli
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 	"time"
+
+	"github.com/mattn/go-shellwords"
+	"github.com/pkg/errors"
 
 	"github.com/pescuma/go-fs-snapshot/lib"
 )
@@ -13,35 +20,28 @@ type backupCmd struct {
 	ProviderID string        `help:"Select which provider to use."`
 	Timeout    time.Duration `help:"Timeout to create snapshot."`
 	Simple     bool          `help:"Try to do it as simple as possible, but not simpler. In Windows this means do not use VSS Writers."`
+	Exec       string        `short:"e" help:"Command to execute after taking the snapshot. The snaphshot path(s) will be added to the end. If not set, this command waits for user input before deleting the snapshot(s)."`
+	NoShell    bool          `help:"Do not pass the exec command to the shell to execute."`
 }
 
 func (c *backupCmd) Run(ctx *context) error {
-	backuper, err := ctx.snapshoter.StartBackup(&fs_snapshot.CreateSnapshotOptions{
-		ProviderID: c.ProviderID,
-		Timeout:    c.Timeout,
-		Simple:     c.Simple,
-		InfoCallback: func(level fs_snapshot.MessageLevel, msg string) {
-			switch level {
-			case fs_snapshot.InfoLevel:
-				if ctx.globals.Verbose >= 1 {
-					fmt.Println("[INFO] " + msg)
-				}
-			case fs_snapshot.DetailsLevel:
-				if ctx.globals.Verbose >= 2 {
-					fmt.Println("[DETAILS] " + msg)
-				}
-			case fs_snapshot.TraceLevel:
-				if ctx.globals.Verbose >= 3 {
-					fmt.Println("[TRACE] " + msg)
-				}
-			}
-		},
-	})
-	defer backuper.Close()
-
+	cmd, err := c.createExecCommand()
 	if err != nil {
 		return err
 	}
+
+	backuper, err := ctx.snapshoter.StartBackup(&fs_snapshot.CreateSnapshotOptions{
+		ProviderID:   c.ProviderID,
+		Timeout:      c.Timeout,
+		Simple:       c.Simple,
+		InfoCallback: c.outputMessages(ctx),
+	})
+	defer backuper.Close()
+	if err != nil {
+		return err
+	}
+
+	var snapshotPaths []string
 
 	for _, dir := range c.Dirs {
 		snapshotPath, err := backuper.TryToCreateTemporarySnapshot(dir)
@@ -51,15 +51,83 @@ func (c *backupCmd) Run(ctx *context) error {
 		case snapshotPath == dir:
 			fmt.Printf("%v: Snapshots not supported for this folder\n", dir)
 		default:
-			fmt.Printf("%v: use snapshot path %v\n", dir, snapshotPath)
+			fmt.Printf("%v: Snapshot path is %v\n", dir, snapshotPath)
 		}
+
+		snapshotPaths = append(snapshotPaths, snapshotPath)
 	}
 
 	fmt.Printf("\n")
-	fmt.Printf("Press <enter> to finish backup and delete snapshot(s)\n")
 
-	var response string
-	_, _ = fmt.Scanln(&response)
+	if cmd != nil {
+		cmd.Args = append(cmd.Args, snapshotPaths...)
 
-	return nil
+		if ctx.globals.Verbose >= 1 {
+			fmt.Printf("Executing: '%v'\n", strings.Join(cmd.Args, "' '"))
+			fmt.Printf("\n")
+		}
+
+		err = cmd.Run()
+		fmt.Printf("\n")
+
+		return errors.Wrapf(err, "Error executing command")
+
+	} else {
+		fmt.Printf("Press <enter> to finish backup and delete snapshot(s)\n")
+
+		var response string
+		_, _ = fmt.Scanln(&response)
+
+		return nil
+	}
+}
+
+func (c *backupCmd) outputMessages(ctx *context) func(level fs_snapshot.MessageLevel, msg string) {
+	return func(level fs_snapshot.MessageLevel, msg string) {
+		switch level {
+		case fs_snapshot.InfoLevel:
+			if ctx.globals.Verbose >= 1 {
+				fmt.Println(msg)
+			}
+		case fs_snapshot.DetailsLevel:
+			if ctx.globals.Verbose >= 2 {
+				fmt.Println("[DETAILS] " + msg)
+			}
+		case fs_snapshot.TraceLevel:
+			if ctx.globals.Verbose >= 3 {
+				fmt.Println("[TRACE] " + msg)
+			}
+		}
+	}
+}
+
+func (c *backupCmd) createExecCommand() (*exec.Cmd, error) {
+	if c.Exec == "" {
+		return nil, nil
+	}
+
+	envs, args, err := shellwords.ParseWithEnvs(c.Exec)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error parsing command to execute")
+	}
+
+	if !c.NoShell {
+		if runtime.GOOS == "windows" {
+			args = append([]string{os.Getenv("COMSPEC"), "/c"}, args...)
+		} else {
+			args = append([]string{"sh", "-e"}, args...)
+		}
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+	if cmd.Err != nil {
+		return nil, cmd.Err
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = append(os.Environ(), envs...)
+
+	return cmd, nil
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/kong v0.6.1
 	github.com/alexeyco/simpletable v1.0.0
 	github.com/go-ole/go-ole v1.2.6
+	github.com/mattn/go-shellwords v1.0.12
 	github.com/pkg/errors v0.9.1
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab
 )
