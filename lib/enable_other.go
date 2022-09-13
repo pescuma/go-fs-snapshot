b3a4260f59e5d723dbd66b0d4b26066c6f7d02d4
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2022-09-12 22:40:26 -0400

    Fixes for non-windows

--- lib/enable_other.go
+++ lib/enable_other.go
@@ -2,12 +2,14 @@
 
 package fs_snapshot
 
+import "errors"
+
 // CurrentUserCanCreateSnapshots returns information if the current user can create snapshots
-func CurrentUserCanCreateSnapshots(infoCb InfoMessageCallback) (can bool, username string, err error) {
-	return false, "", errors.New("snapshots not supported in this OS")
+func CurrentUserCanCreateSnapshots(infoCb InfoMessageCallback) (bool, error) {
+	return false, errors.New("snapshots not supported in this OS")
 }
 
-// EnableSnapshotsForUser enables the current user to run snaphsots.
+// EnableSnapshotsForUser enables the current user to run snapshots.
 // This generally must be run from a prompt with elevated privileges (root or administrator).
 func EnableSnapshotsForUser(username string, infoCb InfoMessageCallback) error {
 	return errors.New("snapshots not supported in this OS")
